787f3609da8f3dc2b8dd93ec58b0d396b8347ecf
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-25 21:27:59 -0600

    annotations

---
+++ features/annotations/variable.ts
@@ -0,0 +1,43 @@
+// : number is our type annotation. 
+// const apples: number = true ; //this line wont work since apples are number
+let apples: number = 5;
+
+let speed: string = 'fast';
+
+//if we give speed another value it would give us an error
+// speed  = true;
+
+let nothingAtAll : null = null;
+
+let undefinedVar : undefined = undefined;
+
+//built in objects
+
+let now: Date = new Date();
+
+//Arrays
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+//Array of numbers
+
+let myNumbers : number[] = [1,2,3];
+
+let truths: boolean [] = [true, true, false];
+
+
+//classes 
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+
+//Object Literal
+let point : {x: number; y: number} = {
+    x: 10,
+    y: 10
+}
+
---
+++ features/types.ts
@@ -0,0 +1,9 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+    age: 20
+}
+//this live will give error
+// person.asdfasdf
+
